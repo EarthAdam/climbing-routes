ddf4d8efcccecd330d7e8c68ca086e4f5b534a7f
OhioAdam <adamsauer.ee@gmail.com>
2022-10-22 12:59:47 -0400

    Update js script

--- js/main.js
+++ js/main.js
@@ -1,10 +1,5 @@
 mapboxgl.accessToken = 'pk.eyJ1IjoiZWFydGhhZGFtIiwiYSI6ImNqd3Y3amlwczBnMzc0YW4xc2x1NWVuNGoifQ.jQvOGeLkupgLxp31-Oa6gw';
 
-//Add commas and stuff to cost value
-function numberWithCommas(x) {
-    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-}
-
 var map = new mapboxgl.Map({
     container: 'map',
     style: 'mapbox://styles/mapbox/satellite-streets-v11',
@@ -21,24 +16,9 @@ var icon = "circle";
 map.on('load', function() {
     var layers = ['7s','8s', '9s'];
     var colors = ['#39DFff','#00FF00', '#FFFF00'];
-    for (i = 0; i < layers.length; i++) {
-        var layer = layers[i];
-        var color = colors[i];
-        var item = document.createElement('div');
-        var key = document.createElement('span');
-        key.className = 'legend-key';
-        key.style.backgroundColor = color;
-      
-        var value = document.createElement('span');
-        value.innerHTML = layer;
-        item.appendChild(key);
-        item.appendChild(value);
-        legend.appendChild(item);
-      }
-    //map.addSource('projects', { type: 'geojson', data: 'projects.geojson' });
-    map.addSource('7s', { type: 'geojson', data: 'https://raw.githubusercontent.com/ohioadam/climbing-routes/master/7s.geojson' });
-    map.addSource('8s', { type: 'geojson', data: 'https://raw.githubusercontent.com/ohioadam/climbing-routes/master/8s.geojson' });
-    map.addSource('9s', { type: 'geojson', data: 'https://raw.githubusercontent.com/ohioadam/climbing-routes/master/9s.geojson' });
+    map.addSource('7s', { type: 'geojson', data: 'https://github.com/OhioAdam/climbing-routes/raw/main/7s.geojson' });
+    map.addSource('8s', { type: 'geojson', data: 'https://github.com/OhioAdam/climbing-routes/blob/main/8s.geojson' });
+    map.addSource('9s', { type: 'geojson', data: 'https://github.com/OhioAdam/climbing-routes/blob/main/9s.geojson' });
     // Add a layer showing the places.
     map.addLayer({
         "id": "7s",
